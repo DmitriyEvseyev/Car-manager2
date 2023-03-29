e5e115eb71b736068df16cdf3cd8a2336942234d
Dmitriy <yevseyevdmitriy@yandex.ru>
2023-03-29 22:58:34 +0400

    src

---
+++ src/controller/IDGenerator.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class IDGenerator {
+    private static IDGenerator instance;
+
+    private int count;
+
+    public static IDGenerator getInstance(int initValue) {
+        if(instance == null) {
+            instance = new IDGenerator(initValue);
+        }
+
+        return instance;
+    }
+
+    private IDGenerator(int count) {
+        this.count = count;
+    }
+
+    public int getId() {
+        return count++;
+    }
+}
+
---
+++ src/exceptions/UpdateCarException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class UpdateCarException extends Exception {
+
+    public UpdateCarException() {
+        super();
+    }
+
+    public UpdateCarException (String message) {
+        super(message);
+    }
+}
